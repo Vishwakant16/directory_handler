bd3196724ce23a7e8213ec3030f507a922e4ea8f
Vaibhavbansal15 <Vaibhavbansal1501@gmail.com>
2022-11-16 18:46:32 +0530

    insertion
    completed insertion function.

--- main.cpp
+++ main.cpp
@@ -1,68 +1,115 @@
 #include <iostream>
 using namespace std;
-class node{
+
+// Structure of Node
+class Node{
 public:
     string Name;//insertion name
-    node* next;//outer boundary
-    node* subFiles;//location of files in folder
-    bool files;//check weather inserted node is representing file or folder
+    Node* next;//outer boundary
+    Node* subFiles;//location of files in folder
+    bool file;//check weather inserted Node is representing file or folder
     //constructor
-    node(string name,string type) {
+    Node(string name,string type) {
         this->Name=name;
         this->next=NULL;
         this->subFiles=NULL;
-        if(type=="files" || type=="Files"){
-            this->files=true;
+        if(type=="file" || type=="File"){
+            this->file=true;
         }
         else{
-            this->files=false;
+            this->file=false;
         }
     }
 };
-node* inserting_folder(node* head,string name){
-    node* new_node=new node(name,"folder");
-    if(!head){
-        head=new_node;
-    }
-    else{
-        new_node->next=head;
-        head=new_node;
-    }
-    return head;
-}
-node* insertion(node* head,string name,string folderName) {
-    //folderName is files(no folder)
-    if (folderName == "no folder" || folderName == "No folder") {
-        node *new_node = new node(name, "files");
-        if (!head) {
-            head = new_node;
-        } else {
-            new_node->next = head;
-            head = new_node;
+
+Node* insertion(Node* head,string name, string type, string folderName) {
+    
+    Node *new_Node = new Node(name, type);    
+    
+    // direct file insertion
+    if (new_Node->file==true) {
+        if(folderName == "NULL"){
+            if (!head) {
+                head = new_Node;
+            } 
+            else {
+                new_Node->next = head;
+                head = new_Node;
+            }
+            cout<<endl<<"File Inserted Successfully!";
         }
-        return head;
-    }
-    //insertion of file in folder
-    else {
-        node *temp = head;
-        node *dummy;
-        while (temp) {
-            temp = temp->next;
-            if (temp->Name == folderName) {
-                dummy = temp;
+        
+        // insertion of file inside particular folder
+        else {
+            Node *temp = head;
+            Node *dummy;
+            while (temp) {
+                temp = temp->next;
+                if (temp->Name == folderName) {
+                    dummy = temp;
+                }
+            }
+            if (!dummy->subFiles) {
+                dummy->subFiles = new_Node;
+                // return dummy->subFiles;
+            }   
+            else {
+                new_Node->next = dummy->subFiles;
+                dummy->subFiles = new_Node;
+                // return dummy->subFiles;
             }
         }
-        node *new_node = new node(name, "files");
-        if (!dummy) {
-            dummy->subFiles = new_node;
-            return dummy->subFiles;
-        } else {
-            new_node->next = dummy->subFiles;
-            dummy->subFiles = new_node;
-            return dummy->subFiles;
-        }
+        cout<<endl<<"File Inserted Successfully!";
     }
+    // folder insertion
+    else{
+        if(!head){
+            head=new_Node;
+        }
+        else{
+            new_Node->next=head;
+            head=new_Node;
+        }
+        cout<<endl<<"Folder inserted successfully!";
+    }
+    return head;  
 }
+
+// Main function
 int main(){
+    Node * head=NULL;
+    string fName;
+    int i;
+    cout<<"Press 1 to insert Folder."<<endl;
+    cout<<"Press 2 to insert File."<<endl;
+    cin>>i;
+    switch(i){
+        case 1:
+            cout<<endl<<"Enter the name of the folder to be inserted: ";
+            cin>>fName;
+            insertion(head,fName,"folder","NULL");
+            break;
+        case 2:
+            char ch;
+            cout<<endl<<"Entre the name of the file to be inserted: ";
+            cin>>fName;
+            cout<<endl<<"Press a to insert a file directly.";
+            cout<<endl<<"Press b to insert file inside any existing folder.";
+            cin>>ch;
+            switch(ch){
+                case 'a':
+                    insertion(head,fName,"file","NULL");
+                    break;
+                case 'b':
+                    string folderName;
+                    cout<<endl<<"Enter the folder name: ";
+                    cin>>folderName;
+                    insertion(head,fName,"file",folderName);
+                    break;
+            }
+            break;
+        default:
+            cout<<endl<<"Invalid Input!";
+        }     
     return 0;
 }
